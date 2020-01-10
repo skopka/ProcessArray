f099c82c7edaa129fad8d2d9061b61d3a4de0432
Aleksey Skopintsev <skopka@skopka.ru>
2020-01-10 14:43:52 +0300

    Add project files.

---
+++ SortArray/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace SortArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Program program = new Program();
+                program.Test();
+            }
+            catch (Exception e)
+            {
+                WriteLine(e);
+            }
+            finally
+            {
+                WriteLine($"Press any key for exit");
+                ReadKey();
+            }
+        }
+
+        private void Test()
+        {
+            int minValue = 14;
+            int[] testArray = { 1, 17, 20, 15, 3, 2, 35, 21, 7, minValue };
+
+            WriteLine($"Min value = {minValue}");
+            PrintArray(testArray, "Source array:");
+
+            int[] processedArray = ProcessArray(testArray, minValue);
+            PrintArray(processedArray, "New array:");
+        }
+
+        private int[] ProcessArray(int[] sourceArray, int minValue)
+        {
+            var result = sourceArray.Where(x => x > minValue).ToArray();
+            Array.Sort(result, new Comparison<int>(
+                (x1, x2) => x2.CompareTo(x1)
+            ));
+
+            return result;
+        }
+
+        private void PrintArray<T>(T[] arr, string text)
+        {
+            WriteLine(text);
+
+            for (int i = 0; i < arr.Length; i++)
+                Write($"{arr[i]} ");
+
+            WriteLine();
+        }
+    }
+}
